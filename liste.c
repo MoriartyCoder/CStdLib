d029a848aea4e057f6cdeca4adfa6fb7621c1df5
MoriartyCoder <DanTronnier@hotmail.de>
2024-06-06 15:52:41 +0200

    Made changes to listAdd; Solved an error for detecting the need to resize

--- liste.c
+++ liste.c
@@ -2,45 +2,45 @@
 
 
 extern LIST* listNew(const int iSize, const int iIncrement, const int iLength) {
-    if(iSize > 0 && iIncrement > 0 && iLength > 0) {
-        LIST *li_list = NULL;
+  if(iSize > 0 && iIncrement > 0 && iLength > 0) {
+    LIST *li_list = NULL;
 
-        li_list = calloc(1, sizeof(LIST));
+    li_list = calloc(1, sizeof(LIST));
 
-        li_list->iCount = 0;        // Anzahl der Elemente in der Liste
-        li_list->iSize = iSize;     // Anzahl der Elemente
-        li_list->iIncrement = iIncrement; // Wenn die Liste voll ist, waechst sie um diese Anzahl
-        li_list->iLength = iLength; // Groesse eines Elementes
+    li_list->iCount = 0;        // Anzahl der Elemente in der Liste
+    li_list->iSize = iSize;     // Anzahl der Elemente
+    li_list->iIncrement = iIncrement; // Wenn die Liste voll ist, waechst sie um diese Anzahl
+    li_list->iLength = iLength; // Groesse eines Elementes
 
-        li_list->p_data = calloc(iSize, iLength);
-        return li_list;
-    }
+    li_list->p_data = calloc(iSize, iLength);
+    return li_list;
+  }
 
-    return NULL;
+  return NULL;
 }
 
 extern int listIncrement(const LIST *li_list) {
-    return li_list != NULL ? (int)li_list->iIncrement : 0;
+  return li_list != NULL ? (int)li_list->iIncrement : 0;
 }
 
 extern int listLength(const LIST *li_list) {
-    return li_list != NULL ? (int)li_list->iLength : 0;
+  return li_list != NULL ? (int)li_list->iLength : 0;
 }
 
 extern int listSize(const LIST *li_list) {
-    return li_list != NULL ? (int)li_list->iSize : 0;
+  return li_list != NULL ? (int)li_list->iSize : 0;
 }
 
 extern int listCount(const LIST *li_list) {
-    return li_list != NULL ? (int)li_list->iCount : 0;
+  return li_list != NULL ? (int)li_list->iCount : 0;
 }
 
 extern void listpfree(LIST **li_list) {
-    if(li_list) {
-        free((*li_list)->p_data);
-        free(*li_list);
-        *li_list = NULL;
-    }
+  if(li_list) {
+    free((*li_list)->p_data);
+    free(*li_list);
+    *li_list = NULL;
+  }
 }
 
 
@@ -60,7 +60,7 @@ extern void listResize(LIST *li_list, const int iResize) {
     iSize = iNewSize;
   }
 
-  memmove(li_listNew->p_data, li_list->p_data, iSize * li_list->iSize);
+  memcpy(li_listNew->p_data, li_list->p_data, iSize * li_list->iSize);
 
   listpfree(&li_list);
 
@@ -77,34 +77,37 @@ extern void* listGet(LIST *li_list, const int iIndex) {
 }
 
 extern void* listGetFromBack(LIST *li_list, const int iIndex) {
-    if(li_list && iIndex >= 0 && (iIndex < li_list->iCount || li_list->iCount == 0)) {
-      return li_list->p_data + (li_list->iCount - iIndex - 1 * li_list->iLength);
-    }
-    return NULL;
+  if(li_list && iIndex >= 0 && (iIndex < li_list->iCount || li_list->iCount == 0)) {
+    return li_list->p_data + (li_list->iCount - iIndex - 1 * li_list->iLength);
+  }
+  return NULL;
 }
 
 extern void* listGetLast(const LIST *li_list) {
-    if(li_list && li_list->iCount > 0) {
-        return li_list->p_data + (li_list->iCount - 1) * li_list->iLength;
-    }
-    return NULL;
+  if(li_list && li_list->iCount > 0) {
+    return li_list->p_data + (li_list->iCount - 1) * li_list->iLength;
+  }
+  return NULL;
 }
 
 extern void* listGetFirst(const LIST *li_list) {
-    if(li_list && li_list->iCount > 0) {
-        return li_list->p_data;
-    }
-    return NULL;
+  if(li_list && li_list->iCount > 0) {
+    return li_list->p_data;
+  }
+  return NULL;
 }
 
 extern void* listAdd(LIST *li_list, void *p_data) {
-    if(li_list && p_data) {
-        return NULL;
-    }
+  if(li_list == NULL || p_data == NULL) {
+    return NULL;
+  }
 
-    if(li_list->iSize - li_list->iCount > 0) {
-        // RESIZE
-    }
-    
-    return memcpy(li_list->p_data + ((li_list->iCount++) * li_list->iLength), p_data, li_list->iLength);
+  printf("L: %d  C: %d\n", li_list->iLength, li_list->iCount);
+  if(li_list->iLength - li_list->iCount == 0) {
+    printf("Pre resize! %p\n", li_list); 
+    listResize(li_list, li_list->iLength + li_list->iIncrement);
+    printf("After resize! %p\n", li_list); 
+  }
+
+  return memcpy(li_list->p_data + ((li_list->iCount++) * li_list->iLength), p_data, li_list->iLength);
 }
