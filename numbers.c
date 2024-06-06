410984d2f46155b91e394c614c6eee5613683f7f
MoriartyCoder <DanTronnier@hotmail.de>
2024-06-06 15:51:12 +0200

    Added lib for numbers

---
+++ numbers.c
@@ -0,0 +1,20 @@
+extern int countDigits(long long llNumber) {
+  int iCount = 0;
+
+  // Berücksichtige negative Zahlen
+  if (llNumber == 0) {
+    return 1;
+  }
+
+  if (llNumber < 0) {
+    llNumber = -llNumber;
+  }
+
+  // Zähle die Anzahl der Stellen
+  while (llNumber != 0) {
+    llNumber /= 10;
+    iCount++;
+  }
+
+  return iCount;
+}
---
+++ numbers.h
@@ -0,0 +1,6 @@
+#ifndef NUMBERS_H
+#define NUMBERS_H
+
+extern int countDigits(long long llNumber);
+
+#endif
