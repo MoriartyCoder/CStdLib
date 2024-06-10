7ff75217c902bfae3eed2576e73c30a94f099056
MoriartyCoder <DanTronnier@hotmail.de>
2024-06-10 19:34:00 +0200

    list reworked; no idea if this works

--- liste.c
+++ liste.c
@@ -2,36 +2,36 @@
 
 
 extern LIST *listNew(const int iDataSize, const int iCapacity, const int iIncrement) {
-    LIST *li_list = NULL;
-    if (iDataSize <= 0 && iIncrement <= 0 && iCapacity <= 0) {
-        return NULL;
-    }
+  LIST *li_list = NULL;
+  if (iDataSize <= 0 && iIncrement <= 0 && iCapacity <= 0) {
+    return NULL;
+  }
 
-    li_list = calloc(1, sizeof(LIST));
+  li_list = calloc(1, sizeof(LIST));
 
-    li_list->iLength    = 0;           
-    li_list->iCapacity  = iCapacity;  
-    li_list->iIncrement = iIncrement;  
-    li_list->iDataSize  = iDataSize;  
+  li_list->iLength    = 0;           
+  li_list->iCapacity  = iCapacity;  
+  li_list->iIncrement = iIncrement;  
+  li_list->iDataSize  = iDataSize;  
 
-    li_list->p_data = calloc(iCapacity, iDataSize);
-    return li_list;
+  li_list->p_data = calloc(iCapacity, iDataSize);
+  return li_list;
 }
 
 extern unsigned int getLength(const LIST *li_list) {
-    return li_list != NULL ? li_list->iLength : 0;
+  return li_list != NULL ? li_list->iLength : 0;
 }
 
 extern unsigned int getIncrement(const LIST *li_list) {
-    return li_list != NULL ? li_list->iIncrement : 0;
+  return li_list != NULL ? li_list->iIncrement : 0;
 }
 
 extern unsigned int getCapacity(const LIST *li_list) {
-    return li_list != NULL ? li_list->iCapacity : 0;
+  return li_list != NULL ? li_list->iCapacity : 0;
 }
 
 extern unsigned int getDataSize(const LIST *li_list) {
-    return li_list != NULL ? li_list->iDataSize : 0;
+  return li_list != NULL ? li_list->iDataSize : 0;
 }
 
 extern void listPfree(LIST **li__list) {
@@ -39,17 +39,17 @@ extern void listPfree(LIST **li__list) {
     return;
   }
 
-    free((*li__list)->p_data);
-    free(*li__list);
-    *li__list = NULL;
+  free((*li__list)->p_data);
+  free(*li__list);
+  *li__list = NULL;
 }
 
 
 extern void listResize(LIST *li_list, const int iResize) {
-  void *p_oldData    = NULL;
-  void *p_newData    = NULL;
-  int  iNewSize      = 0;
-  int  iTransferSize = 0;
+  void          *p_oldData    = NULL;
+  void          *p_newData    = NULL;
+  unsigned int  iNewSize      = 0;
+  int           iTransferSize = 0;
 
   p_oldData = li_list->p_data;
   iNewSize  = li_list->iCapacity + iResize;
@@ -68,7 +68,7 @@ extern void listResize(LIST *li_list, const int iResize) {
 }
 
 
-extern void* listGet(LIST *li_list, const int iIndex) {
+extern void* listGet(LIST *li_list, const unsigned int iIndex) {
   if(li_list == NULL || li_list->iLength == 0 || iIndex >= li_list->iLength) {
     return NULL;
   }
@@ -76,7 +76,7 @@ extern void* listGet(LIST *li_list, const int iIndex) {
   return (char*)li_list->p_data + (iIndex * li_list->iLength);
 }
 
-extern void* listGetFromBack(LIST *li_list, const int iIndex) {
+extern void* listGetFromBack(LIST *li_list, const unsigned int iIndex) {
   if(li_list == NULL || li_list->iLength == 0 || iIndex >= li_list->iLength) {
     return NULL;
   }
@@ -127,24 +127,24 @@ extern void* listAdd(LIST *li_list, void *p_data) {
 }
 
 /*
-extern void* listDelete(LIST *li_list, const int iIndex) {
-  void *p_tmp = NULL;
+   extern void* listDelete(LIST *li_list, const int iIndex) {
+   void *p_tmp = NULL;
 
-  if(li_list == NULL || p_data == NULL) {
-    return NULL;
-  }
+   if(li_list == NULL || p_data == NULL) {
+   return NULL;
+   }
 
-  printf("S: %d  C: %d\n", li_list->iCapacity, li_list->iLength);
-  if(li_list->iCapacity - li_list->iLength == 0) {
-    printf("Pre resize! %p\n", li_list->p_data); 
-    listResize(li_list, li_list->iCapacity + li_list->iIncrement);
-    printf("After resize! %p\n", li_list->p_data); 
-  }
+   printf("S: %d  C: %d\n", li_list->iCapacity, li_list->iLength);
+   if(li_list->iCapacity - li_list->iLength == 0) {
+   printf("Pre resize! %p\n", li_list->p_data); 
+   listResize(li_list, li_list->iCapacity + li_list->iIncrement);
+   printf("After resize! %p\n", li_list->p_data); 
+   }
 
-  p_tmp = (char*)li_list->p_data + (li_list->iLength * li_list->iLength);
-  memcpy(p_tmp, p_data, li_list->iLength);
-  li_list->iLength++;
+   p_tmp = (char*)li_list->p_data + (li_list->iLength * li_list->iLength);
+   memcpy(p_tmp, p_data, li_list->iLength);
+   li_list->iLength++;
 
-  return p_tmp;
-}
-*/
+   return p_tmp;
+   }
+ */
--- liste.h
+++ liste.h
@@ -4,11 +4,11 @@
 #include "ostdlib.h"
 
 typedef struct LIST {
-    unsigned int iLength;      // Amount of elements that are in the list
-    unsigned int iCapacity;    // Maximal elements that the current allocated memory space can hold
-    unsigned int iIncrement;   // How much is added when more space is needed
-    unsigned int iDataSize;    // Size of a element
-    void *p_data;
+  unsigned int iLength;      // Amount of elements that are in the list
+  unsigned int iCapacity;    // Maximal elements that the current allocated memory space can hold
+  unsigned int iIncrement;   // How much is added when more space is needed
+  unsigned int iDataSize;    // Size of a element
+  void *p_data;
 } LIST;
 
 
@@ -20,8 +20,8 @@ extern unsigned int getCapacity(const LIST *li_list);
 extern unsigned int getDataSize(const LIST *li_list);
 extern void listPfree(LIST **li__list);
 extern void listResize(LIST *li_list, const int iResize);
-extern void* listGet(LIST *li_list, const int iIndex);
-extern void* listGetFromBack(LIST *li_list, const int iIndex);
+extern void* listGet(LIST *li_list, const unsigned int iIndex);
+extern void* listGetFromBack(LIST *li_list, const unsigned int iIndex);
 extern void* listGetLast(const LIST *li_list);
 extern void* listGetFirst(const LIST *li_list);
 extern void listClear(const LIST *li_list);
